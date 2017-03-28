1b8b562bffa869f91f1655d6701379eb6936321a
wojciechmusiato <wojciech.musiato@gmail.com>
2017-03-28 18:00:06 +0200

    zrobiony generator

--- imagen.c
+++ imagen.c
@@ -1,5 +1,5 @@
 #include "lodepng.h"
-
+#include "read.h"
 #include <stdio.h>
 #include <stdlib.h>
 void encodeOneStep(const char* filename, const unsigned char* image, unsigned width, unsigned height)
@@ -11,27 +11,34 @@ void encodeOneStep(const char* filename, const unsigned char* image, unsigned wi
   if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
 }
 
-int main(int argc, char *argv[])
-{
-  const char* filename = argc > 1 ? argv[1] : "test.png";
+int generate(option *cfg){
+	unsigned width = cfg->width*5, height = cfg->height*5;
+	unsigned char* image = malloc(width * height * 4);
+	unsigned x, y;
+	int i,j;
 
-  unsigned width = 512, height = 512;
-  unsigned char* image = malloc(width * height * 4);
-  unsigned x, y;
-  for(y = 0 ; y < height ; y++ ) 
- 	 for(x = 0; x < width; x++) {
-		image[4 * width * y + 4 * x + 0] = 255;
-		image[4 * width * y + 4 * x + 1] = 255;
-		image[4 * width * y + 4 * x + 2] = 255;
-		image[4 * width * y + 4 * x + 3] = 255;
+	for(y = 0 ; y < height ; y++ ) 
+ 		for(x = 0; x < width; x++) {
+			image[4 * width * y + 4 * x + 0] = 255;
+			image[4 * width * y + 4 * x + 1] = 255;
+			image[4 * width * y + 4 * x + 2] = 255;
+			image[4 * width * y + 4 * x + 3] = 255;
+		}
+	for(i=0;i<cfg->height;i++){
+		for(j=0;i<cfg->width;j++){
+			if(cfg>grid[i][j]=='+'){
+				for(int n=0;n<5;n++){
+					for(int m=0;m<5;m++){
+						image[4 * width * (i*5+n) + 4 * (j*5+m) + 0]  = 255;
+						image[4 * width * (i*5+n) + 4 * (j*5+m) + 0]  = 25;
+						image[4 * width * (i*5+n) + 4 * (j*5+m) + 0]  = 125;
+						image[4 * width * (i*5+n) + 4 * (j*5+m) + 0]  = 255;
+					}
+				}
+			}
+		}
 	}
-  for(y = 0 ; y < height ; y=y+5 ) 
- 	 for(x = 0; x < width; x=x+2) {
-		image[4 * width * y + 4 * x + 0] = 255;
-		image[4 * width * y + 4 * x + 1] = 25;
-		image[4 * width * y + 4 * x + 2] = 125;
-		image[4 * width * y + 4 * x + 3] = 255;
-}
+	
   encodeOneStep(filename, image, width, height);
 
   free(image);
